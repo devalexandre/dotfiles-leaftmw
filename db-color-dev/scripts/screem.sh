86966f59baa5bbeed8e73e8514d56639a8d83f71
devalexandre <alexandre@indev.net.br>
2022-03-11 19:31:49 -0300

    feat: start

---
+++ db-color-dev/scripts/screem.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DVI-D-0 --mode 1360x768 --pos 0x98 --rotate normal --output HDMI-0 --primary --mode 2560x1080 --pos 1360x0 --rotate normal --output DP-0 --off --output DP-1 --off
